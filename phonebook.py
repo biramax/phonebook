29269c8bb179aeb8ee09b7530292ebcd542327e4
Said Gadjiev <ackusha1984@ya.ru>
2023-03-14 21:51:44 +0300

    Телефонная_книга

---
+++ phonebook.py
@@ -0,0 +1,15 @@
+def read_phonebook():
+    with open('file_name.txt', 'r', encoding='utf-8') as file:
+        pass
+
+    return None
+
+def add_contact(data):
+    surname = input("Введите фамилию")
+    name = input("Введите имя")
+    phonebook = input("Введите номер телефона")
+    data = open('file_name.txt', 'a')
+    data.writelines(surname)
+
+def find():
+    return None
