5988f684e71a9c4b15ba2a24aa3136483b2f1398
Said Gadjiev <ackusha1984@ya.ru>
2023-03-15 18:33:16 +0300

    Вывод списка контактов

--- model.py
+++ model.py
@@ -1,12 +1,11 @@
 def read_phonebook():
-    with open('file_name.txt', 'r', encoding='utf-8') as file:
-        pass
-
-    return None
+    # with open('file_name.txt', 'r', encoding='utf-8') as file:
+    #     pass
+    myfile = open("file_name.txt", "r")
+    return myfile.read()
 
 def add_contact(cont):
     data = open('file_name.txt', 'a')
-    print(cont)
     data.writelines(cont['family'])
     data.writelines(' ')
     data.writelines(cont['name'])
@@ -14,14 +13,13 @@ def add_contact(cont):
     data.writelines(cont['phone'])
     data.writelines('\n')
 
-def find():
-    a = 'cool text'
-    print ('text' in a) 
-    return None
+def find(text):
+    with open('file_name.txt', 'r', encoding='utf-8') as file:
+        resultat = []
 
 
-
+# print(read_phonebook())
 # data = {'family': 'Qwerty', 'name': 'ytrewq', 'phone':'phone'}
 # add_contact(data)
-# # data1 = data['name'], data['surname']
-# # print(data1)
+# data1 = data['name'], data['surname']
+# print(data1)
